1664af2774da69fe12b80d1b1d805c203ed90952
awikum <awikum2@illinois.edu>
2018-04-28 13:57:33 -0500

    Linked activities

--- app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/lab12/MainActivity.java
@@ -3,6 +3,9 @@ package edu.illinois.cs.cs125.lab12;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -13,6 +16,12 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 /**
  * Main class for our UI design lab.
@@ -20,10 +29,9 @@ import org.json.JSONObject;
 public final class MainActivity extends AppCompatActivity {
     /** Default logging tag for messages from the main activity. */
     private static final String TAG = "Lab12:Main";
-
     /** Request queue for our API requests. */
     private static RequestQueue requestQueue;
-
+    public String description = "";
     /**
      * Run when this activity comes to the foreground.
      *
@@ -38,9 +46,23 @@ public final class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        startAPICall();
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "Updating Weather");
+                startAPICall();
+            }
+        });
     }
-
+    public void getDescription(final String json) {
+        JsonParser parser = new JsonParser();
+        JsonObject result = parser.parse(json).getAsJsonObject();
+        JsonArray weather = result.get("weather").getAsJsonArray();
+        description = weather.get(0).getAsJsonObject().get("description").getAsString();
+        TextView weatherDes = findViewById(R.id.weatherDes);
+        weatherDes.setText(description);
+    }
     /**
      * Run when this activity is no longer visible.
      */
@@ -63,6 +85,7 @@ public final class MainActivity extends AppCompatActivity {
                         @Override
                         public void onResponse(final JSONObject response) {
                             try {
+                                getDescription(response.toString());
                                 Log.d(TAG, response.toString(2));
                             } catch (JSONException ignored) { }
                         }
